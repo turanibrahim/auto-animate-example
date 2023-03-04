10dbd1b7aac09a85a96499900fbfe460c18bd962
Ibrahim Turan <hello@ibrahimturan.com>
2023-03-04 22:16:15 +0300

    fix: card img width issue

--- src/components/ExampleTwo.vue
+++ src/components/ExampleTwo.vue
@@ -40,7 +40,7 @@ const removeRandom = ():void => {
     <div class="flex flex-col gap-2">
       <template v-for="item in items" :key="item.title">
         <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-          <img class="object-cover w-full rounded-t-lg h-64 w-auto md:h-48 md:rounded-none md:rounded-l-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="">
+          <img class="object-cover basis-1/4 w-full rounded-t-lg h-64 w-auto md:h-48 md:rounded-none md:rounded-l-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="">
           <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{item.title}}</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{item.description}}</p>
